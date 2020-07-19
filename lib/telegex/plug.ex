d73ad62b0b19e4ebbc8a0a633fce3e0eaf5f6354
Hentioe <me@bluerain.io>
2020-07-19 18:25:14 +0000

    Implement the Plug.Handler module

--- lib/telegex/plug.ex
+++ lib/telegex/plug.ex
@@ -2,4 +2,27 @@ defmodule Telegex.Plug do
   @moduledoc """
   Updates processing plug-in.
   """
+
+  defmacro __using__(opts) do
+    if Enum.empty?(opts), do: implement_plug(), else: implement_preset(opts)
+  end
+
+  defp implement_plug do
+    quote do
+      @behaviour Telegex.Plug
+    end
+  end
+
+  defp implement_preset(:handler) do
+    quote do
+      use Telegex.Plug.Handler
+    end
+  end
+
+  @typedoc "Type of state passed between plugins"
+  @type state :: any()
+  @typedoc "The return value type of `Telegex.Plug.call/2`"
+  @type result :: {:ok | :error | :ignored, Telegex.Plug.state()}
+
+  @callback call(state :: state(), update :: Telegex.Model.Update.t()) :: result()
 end
--- lib/telegex/plug/handler.ex
+++ lib/telegex/plug/handler.ex
@@ -2,4 +2,31 @@ defmodule Telegex.Plug.Handler do
   @moduledoc """
   Message processing plug-in.
   """
+
+  @type match_result :: {:match | :nomatch, Telegex.Plug.state()}
+
+  defmacro __using__(_) do
+    quote do
+      use Telegex.Plug
+
+      @behaviour Telegex.Plug.Handler
+
+      @impl true
+      def call(%{message: nil}, state) do
+        {:ok, state}
+      end
+
+      @impl true
+      def call(%{message: message}, state) do
+        case match(message, state) do
+          {:match, state} -> handle(message, state)
+          {:nomatch, state} -> {:ignored, state}
+        end
+      end
+    end
+  end
+
+  @callback match(message :: Telegex.Model.Message.t(), Telegex.Plug.state()) :: match_result()
+  @callback handle(message :: Telegex.Model.Message.t(), Telegex.Plug.state()) ::
+              Telegex.Plug.result()
 end
