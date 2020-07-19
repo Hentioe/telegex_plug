d9be5c6d9f908d9910997170257e4e564239a285
Hentioe <me@bluerain.io>
2020-07-20 06:46:26 +0800

    Release v0.0.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule TelegexPlug.MixProject do
   use Mix.Project
 
   @description "The abstract design of Telegram's update processing."
-  @version "0.0.1"
+  @version "0.0.2"
 
   def project do
     [
